3cd5f82727671a1a6c606d77decd61926f11640e
Ren <ren.soharab@quicksave.fi>
2022-01-19 00:00:39 +0200

    (article): Added Microsoft to acquire activision blizzard

--- components/Img.js
+++ components/Img.js
@@ -5,7 +5,7 @@ const Img = (props) => {
 
     return (
         <>
-            <img src={props.src} alt={props.alt} onClick={() => setShow(true)} className="cursor-pointer" />
+            <img src={props.src} alt={props.alt} onClick={() => setShow(true)} className={`cursor-pointer ${props.className}`} />
             <section className={`${show ? 'opacity-100 visible' : 'opacity-0 invisible max-h-0'} flex fixed top-0 bottom-0 left-0 right-0 w-full h-screen overflow-hidden transition-all z-40`}>
                 <div className="w-full h-screen bg-opacity-25 bg-blackBrand absolute top-0 bottom-0 left-0 right-0 z-40" onClick={() => setShow(false)}></div>
                 <div className="relative z-40 rounded m-auto flex">
