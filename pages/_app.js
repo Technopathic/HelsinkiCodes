32c744be0c2bb93e2be244b337eca3c1969f650b
ren <ren.soharab@quicksave.fi>
2022-01-25 00:41:58 +0200

    more meta fixing

--- pages/_app.js
+++ pages/_app.js
@@ -39,10 +39,12 @@ const App = (props) => {
   return (
     <>
       <Head>
-        <title>{process.env.APP_TITLE}</title>
         <meta charSet="utf-8" />
         <meta content="ie=edge" httpEquiv="x-ua-compatible" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+        <title>{process.env.APP_TITLE}</title>
+
         <meta name="description" content={process.env.APP_DESCRIPTION} />
 
         <meta name="og:title" property="og:title" content={process.env.APP_TITLE} />
@@ -52,13 +54,18 @@ const App = (props) => {
         <meta name="og:description" property="og:description" content={process.env.APP_DESCRIPTION} />
         <meta name="og:url" property="og:url" content={process.env.APP_URL} />
 
+        <meta name="og:image" property="og:image" content={process.env.APP_IMAGE} />
+        <meta name="og:image:secure_url" property="og:image:secure_url" content={process.env.APP_IMAGE} />
         <meta name="og:image:type" property="og:image:type" content={process.env.APP_IMAGE_TYPE} />
         <meta name="og:image:width" property="og:image:width" content={process.env.APP_IMAGE_WIDTH} />
         <meta name="og:image:height" property="og:image:height" content={process.env.APP_IMAGE_HEIGHT} />
 
-        <meta name="og:image" property="og:image" content={process.env.APP_IMAGE} />
-        <meta name="og:image:secure_url" property="og:image:secure_url" content={process.env.APP_IMAGE} />
-        
+        <link rel="apple-touch-icon" sizes="180x180" href={process.env.APP_APPLE_TOUCH_ICON} />
+        <link rel="icon" type="image/png" sizes="16x16" href={process.env.APP_FAVICON_16x16} />
+        <link rel="icon" type="image/png" sizes="32x32" href={process.env.APP_FAVICON_32x32} />
+
+        <link rel="preload" href="/fonts/Inter-Regular.ttf" as="font" type="font/ttf" crossorigin="anonymous" />
+        <link rel="preload" href="/fonts/Inter-Bold.ttf" as="font" type="font/ttf" crossorigin="anonymous" />
       </Head>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang={process.env.APP_LOCALE}>
-                <Head>
-                    <link rel="apple-touch-icon" sizes="180x180" href={process.env.APP_APPLE_TOUCH_ICON} />
-                    <link rel="icon" type="image/png" sizes="16x16" href={process.env.APP_FAVICON_16x16} />
-                    <link rel="icon" type="image/png" sizes="32x32" href={process.env.APP_FAVICON_32x32} />
-
-                    <link rel="preload" href="/fonts/Inter-Regular.ttf" as="font" type="font/ttf" crossorigin="anonymous" />
-                    <link rel="preload" href="/fonts/Inter-Bold.ttf" as="font" type="font/ttf" crossorigin="anonymous" />
-                </Head>
+                <Head />
                 <body className="bg-lightBrandOne">
                     <Main />
                     <NextScript />
